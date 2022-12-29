6efb6ac2b12573093ff0d77c6d0f67eaabb8a866
unknown <anushaivvala@gmail.com>
2022-12-30 00:23:27 +0530

    addes task2

---
+++ task2.js
@@ -0,0 +1,14 @@
+let data={
+    "name":"anusha",
+    "email":"anushaivvala@gmail.com",
+    "phonenumber":"8374643832",
+     "linkedin":"anushaushdn",
+     "github":"svdhfjhfdh",
+     "address":"hdfh,A3",
+     "summary":"full stack trainer",
+     "education":"btech cse",
+     "projects":"online portal for free education and marketing",
+     "hobbies":"reading,writing"
+}
+     console.log[data];
+     
